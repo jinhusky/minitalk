8dcdfdd9e742cb9a62e5cfeaba6fbf975bb997cb
kationg <tkjprof@gmail.com>
2025-05-23 13:38:37 +0800

    server.c still has bugs

--- client.c
+++ client.c
@@ -6,23 +6,47 @@
 /*   By: kationg <kationg@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 22:41:52 by kationg           #+#    #+#             */
-/*   Updated: 2025/05/23 00:45:56 by kationg          ###   ########.fr       */
+/*   Updated: 2025/05/23 12:00:18 by kationg          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
 #include "libft/libft.h"
 
-
+int signal_char(char c, int PID)
+{
+	int	i;
+	int bits = 0;
+	i = 0;
+	while (bits < 8)
+	{
+		char temp = c >> i;
+		if (temp % 2 == 0)
+			kill(PID, SIGUSR1);
+		else 
+			kill(PID, SIGUSR2);
+		i++;
+		bits++;
+	}
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
+
 	if (argc != 3)
 	{
-		ft_printf("Usage: ./minitalk (FID) message");
-		exit(0);
+		ft_printf("Usage: ./minitalk (PID) message");
+		exit(1);
 	}
 	int PID = ft_atoi(argv[1]);
-	ft_printf("%i",PID);
+	char *mssg = argv[2];
+
+	while (*mssg)
+	{
+		signal_char(*mssg, PID);
+		usleep(100);
+		mssg++;
+	}
 
 }
--- server.c
+++ server.c
@@ -6,15 +6,53 @@
 /*   By: kationg <kationg@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 02:18:23 by kationg           #+#    #+#             */
-/*   Updated: 2025/05/23 00:08:32 by kationg          ###   ########.fr       */
+/*   Updated: 2025/05/23 13:30:11 by kationg          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
+#include "libft/ft_printf.h"
 #include "libft/libft.h"
+#include <signal.h>
+
+void signal_handler(int signum, siginfo_t *pid, void *context)
+{
+	static unsigned char character;
+	(void) context;
+	static int bits = 0;
+	if (signum == SIGUSR1)
+	{
+		character <<= 1;
+		character |= 0;
+		write(1, "received 0 bit\n", 15);
+		kill(pid->si_pid, SIGUSR1);
+	}
+	else
+	{
+		character <<= 1;
+		character |= 1;
+		kill(pid->si_pid, SIGUSR2);
+	}
+	bits++;
+	if (bits == 8)
+	{
+		ft_printf("%c", (char)character);
+		bits = 0;
+		character = 0;
+	}
+}
 
 int main(void)
 {
 	ft_printf("%i", getpid());
-
+	struct sigaction act;
+	act.sa_sigaction = &signal_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	while (1)
+	{
+		pause();
+	}
 }
